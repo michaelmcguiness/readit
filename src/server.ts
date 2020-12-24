a78759eaa10e3dc09291ee3ed051a62bd2ef7f2a
Michael McGuiness <michaelmcguiness123@gmail.com>
2020-12-24 12:18:21 -0500

    register and login pages

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import express, { Express } from 'express'
-// import morgan from 'morgan'
+import express from 'express'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -13,13 +14,22 @@ import subRoutes from './routes/subs'
 
 import trim from './middleware/trim'
 
-const app: Express = express()
+const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
-// app.use(morgan('dev'))
+// @ts-ignore
+app.use(morgan('dev'))
 app.use(trim)
 app.use(cookieParser())
+app.use(
+  // @ts-ignore
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  }),
+)
 
 app.get('/', (_, res) => res.send('Hello World'))
 app.use('/api/auth', authRoutes)
